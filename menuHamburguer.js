da6163488c52e42e3e58bd6fb1b649c970b58bdc
Ramon Coelho <95592949+ramoncoelho@users.noreply.github.com>
2022-02-03 09:35:42 -0300

    layout principal

---
+++ menuHamburguer.js
@@ -0,0 +1,9 @@
+const btnMenu = document.getElementById('btn');
+function toggleMenu(event){
+    if(event.type==='touchstart') event.preventDefault()
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active')
+    
+}
+btnMenu.addEventListener('click', toggleMenu);
+btnMenu.addEventListener('touchstart', toggleMenu);
